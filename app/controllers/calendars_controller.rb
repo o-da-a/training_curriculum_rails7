fb0a7ec3221ce385ec620e4b3dfdb91ab876e461
o-da-a <miu.oda.1381@gmail.com>
2025-05-20 15:41:55 +0900

    バリュー値の表記修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -24,9 +24,8 @@ class CalendarsController < ApplicationController
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
     @week_days = []
-
+    
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
@@ -41,7 +40,7 @@ class CalendarsController < ApplicationController
        wday_num = wday_num - 7  
     end
 
-      days = { month: "(@todays_date + x).month", date: "(@todays_date + x).day", plans: "today_plans", wday: "wday_num"}
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num] }
 
       @week_days.push(days)
     end
